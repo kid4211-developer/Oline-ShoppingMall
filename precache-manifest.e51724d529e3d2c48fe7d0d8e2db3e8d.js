da4c7ddc8aa61c56b09e74bb73e7f762233e78f5
kid4211 <kid4211@gmail.com>
2021-03-04 10:51:26 +0900

    Updates

---
+++ precache-manifest.e51724d529e3d2c48fe7d0d8e2db3e8d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589ff9cd9e7a5eb8de704df2c1215ef3",
+    "url": "/Online-ShoppingMall/index.html"
+  },
+  {
+    "revision": "7c9bfc064a26012a8b6e",
+    "url": "/Online-ShoppingMall/static/css/2.1fcff466.chunk.css"
+  },
+  {
+    "revision": "65bd0ad903b420d619a0",
+    "url": "/Online-ShoppingMall/static/css/main.697839eb.chunk.css"
+  },
+  {
+    "revision": "7c9bfc064a26012a8b6e",
+    "url": "/Online-ShoppingMall/static/js/2.45419bed.chunk.js"
+  },
+  {
+    "revision": "9f2b35269cf4efe04e68d9400ccf5296",
+    "url": "/Online-ShoppingMall/static/js/2.45419bed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65bd0ad903b420d619a0",
+    "url": "/Online-ShoppingMall/static/js/main.e8fa177c.chunk.js"
+  },
+  {
+    "revision": "1f0542f9008ff9a5ce2a",
+    "url": "/Online-ShoppingMall/static/js/runtime-main.c31c0489.js"
+  },
+  {
+    "revision": "dd51bdba8eaa4c9ad60db2373bdb624c",
+    "url": "/Online-ShoppingMall/static/media/feature-tile__build.dd51bdba.png"
+  },
+  {
+    "revision": "a07dbc726d395bebdb7769182beaac58",
+    "url": "/Online-ShoppingMall/static/media/feature-tile__platform.a07dbc72.png"
+  },
+  {
+    "revision": "f0b920d6823b7359c3bcf859d69a891a",
+    "url": "/Online-ShoppingMall/static/media/feature-tile__projects.f0b920d6.png"
+  },
+  {
+    "revision": "c1aee00129848d03175f93962bda3153",
+    "url": "/Online-ShoppingMall/static/media/feature-tile__work.c1aee001.png"
+  },
+  {
+    "revision": "b28e3e30962488350fe80888d2833967",
+    "url": "/Online-ShoppingMall/static/media/landingPageImg.b28e3e30.jpg"
+  },
+  {
+    "revision": "ce5e9d2bf35b3049f3fdbf9d54357ecc",
+    "url": "/Online-ShoppingMall/static/media/logo.ce5e9d2b.svg"
+  }
+]);
